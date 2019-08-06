a59a4f4d353e333eb431d460723c279655f181b6
fito <fifodominguez@gmail.com>
2019-08-05 18:28:13 -0600

    Updates

---
+++ precache-manifest.a6676312eb4463b7a5d2e975609e9d6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611eafea7177643947a7e0e329f20c48",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "ba9ef410ed93bd0550f3",
+    "url": "/pokedex/static/css/main.58e562a6.chunk.css"
+  },
+  {
+    "revision": "15735734eacf2a62df05",
+    "url": "/pokedex/static/js/2.e2b58a73.chunk.js"
+  },
+  {
+    "revision": "ba9ef410ed93bd0550f3",
+    "url": "/pokedex/static/js/main.cd21c7d3.chunk.js"
+  },
+  {
+    "revision": "499fa032a833d26aac94",
+    "url": "/pokedex/static/js/runtime~main.fa119ff4.js"
+  },
+  {
+    "revision": "7802853e38d805abb7e6a80e76b1eb3a",
+    "url": "/pokedex/static/media/pokemonlogo.7802853e.png"
+  }
+]);
